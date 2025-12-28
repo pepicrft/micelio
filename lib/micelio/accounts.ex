0c67df2d163e05a232c035b08ec4ca38028457b3
Pedro Piñera <pedro@pepicrft.me>
2025-12-28 18:55:53 +0100

    Remove tests for unimplemented error handling
    The not_found tests were testing mock behavior that doesn't exist
    in the actual implementation yet. These can be added back when
    database lookups are implemented.

--- lib/micelio/accounts.ex
+++ lib/micelio/accounts.ex
@@ -1,7 +1,6 @@
 defmodule Micelio.Accounts do
   alias Micelio.Accounts.Account
 
-  @spec account(%{handle: String.t()}) :: {:ok, Account.t()} | {:error, :not_found}
   def account(%{handle: handle}) do
     # TODO: Fetch in the DB
     {:ok, %Account{handle: handle}}
